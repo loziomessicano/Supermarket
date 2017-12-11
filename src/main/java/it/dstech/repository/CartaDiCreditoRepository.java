bdda82c77d44ffc900596cce5fa3fc6ec5394c19
Qwertys90 <mr.kris90@gmail.com>
2017-12-11 13:13:30 +0100

    adriano gay
    importate

---
+++ src/main/java/it/dstech/repository/CartaDiCreditoRepository.java
@@ -0,0 +1,11 @@
+package it.dstech.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.dstech.model.CartaDiCredito;
+
+public interface CartaDiCreditoRepository extends CrudRepository <CartaDiCredito,Integer> {
+	
+	
+
+}
